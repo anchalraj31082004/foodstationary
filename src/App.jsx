33a931fdefce535da2f228c9aa4a8f95b7b3e152
anchalraj31082004 <anchalraj20045@gmail.com>
2023-06-10 16:04:28 +0530

    Order section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import AboutUs from './components/AboutUs'
 import Header from './components/Header'
+import Offer from './components/Offer'
 import Services from './components/Services'
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <Header/>
     <AboutUs/>
     <Services/>
+    <Offer img={`frenchfriesbg.png`}/>
     </>
   )
 }
---
+++ src/components/Offer.jsx
@@ -0,0 +1,19 @@
+import Button from "./Button"
+
+const Offer = (props) => {
+  return (
+    <section className={`bg-[url(/assets/${props.img})] h-[60vh] bg-center bg-cover bg-no-repeat text-[--primary-dark] ${props.fDirection} `}>
+      <div className="flex flex-col gap-4 max-w-lg pt-20 pl-12">
+      <h4 className="text-3xl font-bold">Special</h4>
+      <h2 className="text-5xl font-extrabold">Sale 50% Discound</h2>
+      <h4 className="text-3xl font-bold">ALL FOOD PRODUCTS</h4>
+      <p className="text-lg font-light">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure cumque aspernatur mollitia nisi minus veritatis quia, rem culpa explicabo architecto suscipit est pariatur? Libero, ad</p>
+      </div>
+      <div className="ml-12 mt-8">
+      <Button text="Buy Now"  />
+      </div>
+    </section>
+  )
+}
+
+export default Offer
